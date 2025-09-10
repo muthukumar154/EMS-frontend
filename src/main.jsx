d42923af2ff107b6e9cdf59c2645af7271172424
muthukumar154 <muthukumarcse8@gmail.com>
2025-09-10 11:42:14 +0530

    Update routes to HashRouter and Vite base path for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,38 +5,29 @@ import Dashboard from './Pages/Dashboard.jsx';
 import Employee from './Pages/Employee.jsx';
 import EmployeeDetail from './Pages/EmployeeDetail.jsx';
 import EmployeeAdd from './Pages/EmployeeAdd.jsx';
-import store from './redux/store.js';
-import { Provider } from 'react-redux'
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route
-} from 'react-router-dom';
 import EmployeeEdit from './Pages/EmployeeEdit.jsx';
 import Login from './Pages/Login.jsx';
 import Calender from './Pages/Calender.jsx';
 import Message from './Pages/Message.jsx';
-
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route path='/' element={<Login />} />
-      <Route path='/dashboard' element={<Dashboard />} />
-      <Route path='/employee' element={<Employee />} />
-      <Route path='/calendar' element={<Calender />} />
-      <Route path='/message' element={<Message />} />
-      <Route path='/employee/new' element={<EmployeeAdd />} />
-      <Route path='/employee/:id' element={<EmployeeDetail />} />
-      <Route path='/employee/edit/:id' element={<EmployeeEdit />} />
-    </Route>
-  )
-)
+import store from './redux/store.js';
+import { Provider } from 'react-redux'
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Login />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="employee" element={<Employee />} />
+          <Route path="calendar" element={<Calender />} />
+          <Route path="message" element={<Message />} />
+          <Route path="employee/new" element={<EmployeeAdd />} />
+          <Route path="employee/:id" element={<EmployeeDetail />} />
+          <Route path="employee/edit/:id" element={<EmployeeEdit />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   </Provider>
-
 )
